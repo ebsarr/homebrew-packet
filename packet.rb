bd0f357b91fea99b7b38b74ffdbf604d1ba4ac47
bassirou-sarr <tsub1208>
2018-06-04 00:05:25 +0900

    updated formulae for packet v2.3

---
+++ packet.rb
@@ -0,0 +1,15 @@
+class Packet < Formula
+	desc "A CLI tool to manage packet.net services"
+	homepage "https://packetcli.ebsarr.com"
+	url "https://github.com/ebsarr/packet/blob/v2.3/archive/packet-2.3.tar.gz"
+	sha256 "4af654480f30fd73bedd47c52a76667341ca9fd950aa3bb02b7201490c9c2579"
+	version "2.3"
+  
+	# depends_on "curl"
+  
+	bottle :unneeded
+  
+	def install
+	  bin.install "packet"
+	end
+  end
